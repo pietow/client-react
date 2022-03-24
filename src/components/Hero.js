ba150d82ef7b72fd1afac88a20df39335d688c04
Sonny Stark <sonny.stark@posteo.net>
2022-03-24 10:58:16 +0100

    style: tailwind config

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section className="md:flex md:flex-row w-full h-screen bg-primary bg-hero bg-cover bg-center">
-            <div className="text-center">
-              <h1 className="md:flex md:flex-row ">This is the HERO section</h1>
+        <section className="container md:flex md:flex-row w-full h-screen bg-hero bg-cover ">
+            <div className="md:flex md:flex-col content-center border-2">
+              <h1 className="">Welcome to COMPANY NAME</h1>
               <p>Some text goes here</p>
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Click here</button>
                
