ba665d290880627c3baa01f9d927c48f2bcaa534
gino <gino.massalski@gmail.com>
2022-03-24 14:27:03 +0100

    feat: navbar tryout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Features from '../components/Features/'
+import Navbar from '../components/Navbar'
 
 
 export default function Home({toggle}) {
   return (
-    <main className={toggle ? "blur" : ""}>
+    <main className={toggle ? "transition duration-300 blur" : "transition duration-300"}>
+        <Navbar />
         <Hero />
         <Features />
     </main>
