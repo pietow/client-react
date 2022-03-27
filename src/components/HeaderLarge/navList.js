7a0c633c8e3b19b38b10d66119380f7bef15d786
gino <gino.massalski@gmail.com>
2022-03-27 20:14:52 +0200

    feat: large header style done

--- src/components/HeaderLarge/navList.js
+++ src/components/HeaderLarge/navList.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function NavList() {
     
-  const links = [['/', 'home'], ['/register', 'register'], ['/login', 'login'], ['/contact', 'contact']]
+  const links = [['/register', 'register'], ['/login', 'login'], ['/contact', 'contact']]
     
   return (
-    <nav>
-        <ul className="flex flex-row justify-between text-3xl font-noto">
-            {links.map((elem, i) => <li key={i} className=""><a className="" href={elem[0]}>{elem[1]}</a></li>)}
+    <nav className='border w-72 mt-2 bg-teal-normal py-3 px-5 rounded-full shadow-gray-dark'>
+        <ul className="flex flex-row justify-between text-2xl font-noto">
+            {links.map((elem, i) => <li key={i} className="hover:scale-110"><a className="" href={elem[0]}>{elem[1]}</a></li>)}
         </ul>
     </nav>
   )
