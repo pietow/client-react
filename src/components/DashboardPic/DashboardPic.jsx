7c47b95b69c42b79627100cdc634b1b656241ed5
gino <gino.massalski@gmail.com>
2022-03-28 14:47:30 +0200

    chore: refactor

--- src/components/DashboardPic/DashboardPic.jsx
+++ src/components/DashboardPic/DashboardPic.jsx
@@ -4,7 +4,7 @@ export default function DashboardPic({randomData}) {
   return (
     <figure className="w-40 space-y-2 m-6">
         <img className="drop-shadow-lg rounded-full" src={'https://picsum.photos/200/200.jpg'} alt="lorem"/>
-        <figcaption className="text-center" >inner width: {window.innerWidth}<br /> random data id: {randomData[0]?.fname}</figcaption>
+        <figcaption className="text-center" >inner width: {window.innerWidth}<br /> random data id: {randomData}</figcaption>
     </figure>
   )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Logo from '../components/Logo'
-import PrevNextButtons from '../components/PrevNextButtons'
+// import PrevNextButtons from '../components/PrevNextButtons'
 import DashboardSection from '../components/DashboardSection'
 import {useState} from 'react'
 import DashboardPic from '../components/DashboardPic'
+import NextButton from '../components/PrevNextButtons/NextButton'
+import PrevButton from '../components/PrevNextButtons/PrevButton'
 
 export default function Dashboard() {
 
   const [randomData, setRandomData] = useState([]);
+
 
   const getUser = async () => {
     const response = await fetch('api/users/', {
@@ -19,15 +22,18 @@ export default function Dashboard() {
     setRandomData(result)
   }
 
-  console.log(randomData)
-
   return (
     <div className="flex flex-col items-center space-y-10 bg-fixed bg-gradient-to-t from-pistachio-normal to-apricot-bright"> 
-      <DashboardPic randomData={randomData}/>
-        <button onClick={getUser} className="active:bg-light-orange hover:outline-1 m-1 hover:drop-shadow hover:outline-teal-dark rounded-md bg-pistachio-normal p-2">fetch data</button>
-
-      <DashboardSection randomData={randomData}/>
-      <PrevNextButtons /> 
+      <div className='flex items-center'>
+        <PrevButton />
+        <div>
+          <DashboardPic randomData={randomData[0]?.fname}/>
+        </div>
+        <NextButton />
+      </div>
+          <button onClick={getUser} className="active:bg-light-orange hover:outline-1 m-1 hover:drop-shadow hover:outline-teal-dark rounded-md bg-pistachio-normal p-2">fetch data</button>
+      <DashboardSection heading={'name'} randomData={randomData[0]?.fname + ' ' + randomData[0]?.lname}/>
+      <DashboardSection heading={'email address'} randomData={randomData[0]?.email}/>
       <Logo />
     </div>
   )
