a3b3ed4a4a7883abcda012dfaecb62c775b4ff50
gino <gino.massalski@gmail.com>
2022-03-29 19:56:06 +0200

    feat: style dashboard

--- src/components/DashboardSection/DashboardSection.jsx
+++ src/components/DashboardSection/DashboardSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function DashboardSection({randomData, heading}) {
   return (
-    <section className="flex flex-col items-center bg-apricot-bright m-4 rounded-md drop-shadow-md">
-        <h1 className="font-zeyada m-4 text-3xl">{heading}</h1>
+    <section className="flex flex-col items-center backdrop-blur m-4 rounded-md drop-shadow-md border border-best-white rounded-md">
+        <h1 className="font-zeyada text-best-white m-4 text-3xl">{heading}</h1>
         <p className="rounded-md mx-4 p-2 bg-apricot-bright opacity-80">{randomData}</p>
       </section>
   )
