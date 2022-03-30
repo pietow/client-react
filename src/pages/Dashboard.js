753f466fd4a35e16f876ffd7db576884776afdec
gino <gino.massalski@gmail.com>
2022-03-30 09:24:51 +0200

    style: fix text-center in heading

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -34,7 +34,7 @@ export default function Dashboard() {
       <DashboardSection heading={'hi, i am'} randomData={'Heinz Ketchup'}/>
       <DashboardSection heading={'about me'} randomData='maybe email address or some other stuff. bio could work. or places one has already seen. hobbies is an alternative. maybe one is a good chef and welcomes people to have a nice meal together. By the way, this text is set in <text-justify> so that the right side of the text is also nicely aligned. I just want to see how this paragraph works with a lot of text in it. And lorem ipsum is boring! etc etc etc'/>
       <DashboardSection heading={'about my dear guests'} randomData='My guests shall pay a lot and do the dishes twice a day. My fridge needs to be filled at least every other day past noon. I like to sleep long so NO KIDS. I snore though.' />
-      <DashboardSection heading={'tongues i might understand'} randomData={'french, japanese, german, english, dutch (maybe insert tiny icon flags here)'}/>
+      <DashboardSection heading={'i speak'} randomData={'french, japanese, german, english, dutch (maybe insert tiny icon flags here)'}/>
       <PrevNextButtons />
       <div className="bg-best-white opacity-40 w-fit my-20 scale-[2] border border-best-white rounded-full">
         <a href="/"><Logo /></a>
