08f74cbc075242372b935fb0d54e02760a8c8adf
gino <gino.massalski@gmail.com>
2022-03-31 15:16:06 +0200

    chore: shorten feature component

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,80 +6,41 @@ import CardImage2 from '../../assets/img/jaunt-and-joy-nmi2TJCW-BM-unsplash.jpg'
 import CardImage3 from '../../assets/img/jonas-verstuyft-jXwZOSXC7KA-unsplash.jpg'
 import CardImage4 from '../../assets/img/mukuko-studio-tPKQwYHy8q4-unsplash.jpg'
 
+
 export default function Features() {
+
+    const images = [
+        [CardImage1, "Be spontaneous"],
+        [CardImage2, "Start friendships"],
+        [CardImage3, "Learn from a local"],
+        [CardImage4, "Sustainability"]
+    ]
+
     return (
-      <main className="bg-gradient-to-t from-pistachio-normal to-apricot-bright">
-      <h1 className="text-center font-zeyada p-6">Connect and make new memories</h1>
-        <div className="flex flex-row justify-evenly mt-10 pb-32">
-          
-            <div className="justify-center">
-                <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
-                    <img className="rounded-t-lg" src={CardImage1} alt="" />
-                    <div className="p-6">
-                        <h2 className="mb-2">
-                            Be spontaneous
-                        </h2>
-                        <p className="font-noto text-gray-700 text-base mb-4">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content. Some
-                            quick example text to build on the card title and
-                            make up the bulk of the card's content.
-                        </p>
+        <main className="bg-gradient-to-t from-pistachio-normal to-apricot-bright">
+            <h1 className="text-center p-6">Connect and make new memories</h1>
+            <div className="flex flex-row justify-evenly mt-10 pb-32">
+            {images.map((elem, i) => (
+                <div className="justify-center" key={elem[1]}>
+                    <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
+                        <figure>
+                            <img className="rounded-t-lg" src={elem[0]} alt="this is a pic" />
+                        </figure>
+                        <div className="p-6">
+                            <h2 className="mb-2">
+                                {elem[1]}
+                            </h2>
+                            <p className="text-base mb-4">
+                                Some quick example text to build on the card title
+                                and make up the bulk of the card's content. Some
+                                quick example text to build on the card title and
+                                make up the bulk of the card's content.
+                            </p>
+                        </div>
                     </div>
                 </div>
+            ))}
             </div>
-
-            <div className="justify-center">
-                <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
-                    <img className="rounded-t-lg" src={CardImage2} alt="" />
-                    <div className="p-6">
-                    <h2 className="mb-2">
-                            Start friendships
-                        </h2>
-                        <p className="font-noto text-gray-700 text-base mb-4">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content. Some
-                            quick example text to build on the card title and
-                            make up the bulk of the card's content.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="justify-center">
-                <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
-                    <img className="rounded-t-lg" src={CardImage3} alt="" />
-                    <div className="p-6">
-                    <h2 className="mb-2">
-                            Learn from a local
-                        </h2>
-                        <p className="font-noto text-gray-700 text-base mb-4">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content. Some
-                            quick example text to build on the card title and
-                            make up the bulk of the card's content.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="justify-center">
-                <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
-                    <img className="rounded-t-lg" src={CardImage4} alt="" />
-                    <div className="p-6">
-                    <h2 className="mb-2">
-                            Sustainability
-                        </h2>
-                        <p className="font-noto text-gray-700 text-base mb-4">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content. Some
-                            quick example text to build on the card title and
-                            make up the bulk of the card's content.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
         </main>
     )
 }
