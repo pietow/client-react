14e9c71b91bac9f7efe7d4cba4ec15c0f584c2c5
gino <gino.massalski@gmail.com>
2022-03-31 13:16:46 +0200

    chore: tryout cleaning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 /** @format */
 
-// import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import linksFooter from '../../data/linksFooter.json'
@@ -30,3 +29,27 @@ export default function Footer() {
         </footer>
     )
 }
+
+// export default function Footer() {
+//     return (
+//         <footer className="bg-pistachio-dark pb-14 pt-4 h-14 border-t-2 border-teal-dark flex md:flex-row justify-center md:mx-auto space-x-0 md:space-x-8 space-y-8 md:space-y-0 order-3 md:order-2">
+//                 {linksFooter.map((item) => (
+//                     <div className="flex flex-row" key={item.title}>
+//                         <div>
+//                             {item.links.map((link) => (
+//                                 <Link
+//                                     to={link.path}
+//                                     key={link.name}
+//                                     className="text-sm uppercase text-gray-dark font-noto px-2">
+//                                     {link.name}
+//                                 </Link>
+//                             ))}
+//                         </div>
+//                     </div>
+//                 ))}
+//             <p className="text-sm text-center">
+//                 Copyright bla bla and photo credits here
+//             </p>
+//         </footer>
+//     )
+// }
