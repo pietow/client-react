3c81499c701af459255fcf8d922efab0c831bfa2
gino <gino.massalski@gmail.com>
2022-04-01 09:21:31 +0200

    style: add logo at end of landing page

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,6 +5,7 @@ import CardImage1 from '../../assets/img/tobias-tullius-pb7gfl7_Ni8-unsplash.jpg
 import CardImage2 from '../../assets/img/jaunt-and-joy-nmi2TJCW-BM-unsplash.jpg'
 import CardImage3 from '../../assets/img/jonas-verstuyft-jXwZOSXC7KA-unsplash.jpg'
 import CardImage4 from '../../assets/img/mukuko-studio-tPKQwYHy8q4-unsplash.jpg'
+import LogoLink from '../LogoLink'
 
 
 export default function Features() {
@@ -17,9 +18,9 @@ export default function Features() {
     ]
 
     return (
-        <main className="bg-gradient-to-t from-pistachio-normal to-apricot-bright">
-            <h1 className="text-center p-6">Connect and make new memories</h1>
-            <div className="flex flex-row justify-evenly mt-10 pb-32">
+        <main className="bg-gradient-to-t from-pistachio-normal to-apricot-bright flex flex-col items-center pb-20 overflow-hidden">
+            <h1 className="p-6">Connect and make new memories</h1>
+            <div className="flex flex-row justify-evenly mt-10 pb-32 w-screen">
             {images.map(elem => (
                 <div className="justify-center" key={elem[1]}>
                     <div className="rounded-lg shadow-lg bg-best-white max-w-sm">
@@ -30,7 +31,7 @@ export default function Features() {
                             <h2 className="mb-2">
                                 {elem[1]}
                             </h2>
-                            <p className="text-base mb-4">
+                            <p className="text-justify mb-4">
                                 Some quick example text to build on the card title
                                 and make up the bulk of the card's content. Some
                                 quick example text to build on the card title and
@@ -41,6 +42,7 @@ export default function Features() {
                 </div>
             ))}
             </div>
+            <LogoLink />
         </main>
     )
 }
