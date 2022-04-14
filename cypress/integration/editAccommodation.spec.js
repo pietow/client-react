4c14e929a0402900ee7a3d6c2539a5097350bd8d
piet <pprotassow@posteo.de>
2022-04-14 19:37:39 +0200

    test: check if page is available

--- cypress/integration/editAccommodation.spec.js
+++ cypress/integration/editAccommodation.spec.js
@@ -1,3 +1,15 @@
 /** @format */
 
-const bla = 1
+describe('edit Accommodation', function () {
+    it('Display form', function () {
+        cy.visit('http://localhost:3000/offer/host')
+
+        cy.get('[data-testid="yesBox"]').click()
+
+        cy.get('class=hidden')
+
+        {/* cy.get('[data-testid="next"]').click() */}
+
+        {/* cy.url().should('inlcude', '/profile') */}
+    })
+})
