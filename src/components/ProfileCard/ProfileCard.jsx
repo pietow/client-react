c00c7801683c989f85a70c88bb78c51a906e5f59
Sonny Stark <sonny.stark@posteo.net>
2022-04-20 10:50:28 +0200

    Auto stash before checking out "origin/development"
    feat: structure profileCard

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -5,9 +5,25 @@ import React from 'react'
 export default function ProfileCard({ userData }) {
     console.log(userData)
     return (
-        <div className="border-2 p-4 rounded border-best-white">
-            <h1 className="text-best-white">Hello world</h1>
-            <p className="text-best-white">{userData.username}</p>
-        </div>
+        <card className="w-1/5 h-full border-2 border-gray-dark p-4 rounded bg-best-white">
+            <div className="">
+                {/* <figure>Profile image goes here</figure> */}
+                <p className="text-black text-xs border-b-2 p-2">
+                    Username: {userData.username}
+                </p>
+                <p className="text-black text-xs border-b-2 p-2">
+                    Name: {userData.fname + ' ' + userData.lname}
+                </p>
+                <p className="text-black text-xs border-b-2 p-2">
+                    Gender: {userData.gender}
+                </p>
+                <p className="text-black border-b-2 p-2 text-xs">
+                    Age: {userData.birthday}
+                </p>
+                <p className="text-black border-b-2 p-2 text-xs">
+                    Lives in {userData.city + ', ' + userData.country}
+                </p>
+            </div>
+        </card>
     )
 }
