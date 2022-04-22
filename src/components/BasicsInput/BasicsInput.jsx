ced428ca1eeb703b7ecb51c7d3b3976c2dfd31ec
piet <pprotassow@posteo.de>
2022-04-23 01:30:52 +0200

    feat: add states

--- src/components/BasicsInput/BasicsInput.jsx
+++ src/components/BasicsInput/BasicsInput.jsx
@@ -4,23 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { dateArr } from '../../data/datesArray'
 import Birthday from '../Birthday'
 
-export default function BasicsInput({
-    description,
-    savable,
-    setSavable,
-    input,
-    setInput,
-}) {
-    useEffect(() => {
-        setInput((oldInput) => ({ ...oldInput, text: description.text }))
-    }, [setInput, description])
-
-    const styles = {
-        label: 'w-28 text-right text-best-white text-sm',
-        input: 'py-1 bg-best-white text-sm px-2 w-full border border-gray-300 rounded-sm focus:outline-none focus:border-pistachio-dark focus:border-2 shadow-pistachio-dark focus:shadow-lg selection:bg-pistachio-dark',
-        container: 'flex mb-4 items-center',
-    }
-
+export default function BasicsInput({ state, dispatch, styles }) {
     const gender = {
         "I'd rather not tell": '',
         'Female': 'Female',
@@ -30,75 +14,127 @@ export default function BasicsInput({
     }
 
     return (
-        <div className="flex flex-col">
-            <div>
-                <div className={styles.container}>
-                    <label htmlFor="fname" className={styles.label}>
-                        First Name
-                    </label>
-                    <div className="w-6/12 ml-8">
-                        <input
-                            id="fname"
-                            type="text"
-                            className={styles.input}
-                        />
+        <section className="p-4 flex flex-col backdrop-brightness-75 backdrop-blur-lg drop-shadow-md border border-best-white rounded-md">
+            <h1 className="w-full underline underline-offset-8 decoration-1 text-best-white text-4xl">
+                Basics
+            </h1>
+            <div className="flex flex-col">
+                <div>
+                    <div className={styles.container}>
+                        <label htmlFor="fname" className={styles.label}>
+                            First Name
+                        </label>
+                        <div className="w-6/12 ml-8">
+                            <input
+                                id="fname"
+                                type="text"
+                                value={state.fname ?? 'fail'}
+                                onChange={(e) =>
+                                    dispatch({
+                                        type: 'update_user',
+                                        payload: {
+                                            fname: e.target.value,
+                                        },
+                                    })
+                                }
+                                className={styles.input}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className={styles.container}>
-                    <label htmlFor="lname" className={styles.label}>
-                        Last Name
-                    </label>
-                    <div className="w-6/12 ml-8">
-                        <input
-                            id="lname"
-                            type="text"
-                            className={styles.input}
-                        />
+                    <div className={styles.container}>
+                        <label htmlFor="lname" className={styles.label}>
+                            Last Name
+                        </label>
+                        <div className="w-6/12 ml-8">
+                            <input
+                                id="lname"
+                                type="text"
+                                value={state.lname ?? 'fail'}
+                                onChange={(e) =>
+                                    dispatch({
+                                        type: 'update_user',
+                                        payload: {
+                                            lname: e.target.value,
+                                        },
+                                    })
+                                }
+                                className={styles.input}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className={styles.container}>
-                    <label htmlFor="motto" className={styles.label}>
-                        Short tagline
-                    </label>
-                    <div className="w-6/12 ml-8">
-                        <input
-                            id="motto"
-                            type="text"
-                            className={styles.input}
-                        />
-                        <p className="text-best-white text-sm mt-1 selection:bg-pistachio-dark selection:text-gray-900">
-                            What is your mission or life motto?
-                        </p>
+                    <div className={styles.container}>
+                        <label htmlFor="motto" className={styles.label}>
+                            Short tagline
+                        </label>
+                        <div className="w-6/12 ml-8">
+                            <input
+                                id="motto"
+                                type="text"
+                                value={state.profile.motto ?? 'fail'}
+                                onChange={(e) =>
+                                    dispatch({
+                                        type: 'update_profile',
+                                        payload: {
+                                            motto: e.target.value,
+                                        },
+                                    })
+                                }
+                                className={styles.input}
+                            />
+                            <p className="text-best-white text-sm mt-1 selection:bg-pistachio-dark selection:text-gray-900">
+                                What is your mission or life motto?
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.container}>
-                    <label htmlFor="gender" className={styles.label}>
-                        I Am
-                    </label>
-                    <div className="w-6/12 ml-8">
-                        <select
-                            id="gender"
-                            type="text"
-                            className={styles.input}>
-                            {Object.keys(gender).map((key, i) => {
-                                return (
-                                    <option key={i} value={gender[key]}>
-                                        {key}
-                                    </option>
-                                )
-                            })}
-                        </select>
-                        <p className="text-best-white text-sm mt-1 selection:bg-pistachio-dark selection:text-gray-900">
-                            What is your mission or life motto?
-                        </p>
+                    <div className={styles.container}>
+                        <label htmlFor="gender" className={styles.label}>
+                            I Am
+                        </label>
+                        <div className="w-6/12 ml-8">
+                            <select
+                                id="gender"
+                                type="text"
+                                value={state.profile?.gender ?? 'fail'}
+                                onChange={(e) =>
+                                    dispatch({
+                                        type: 'update_profile',
+                                        payload: {
+                                            gender: e.target.value,
+                                        },
+                                    })
+                                }
+                                className={styles.input}>
+                                {Object.keys(gender).map((key, i) => {
+                                    return (
+                                        <option key={i} value={gender[key]}>
+                                            {key}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                            <p className="text-best-white text-sm mt-1 selection:bg-pistachio-dark selection:text-gray-900">
+                                What is your mission or life motto?
+                            </p>
+                        </div>
                     </div>
+                    <Birthday
+                        state={state}
+                        savable={'bla'}
+                        setSavable={'bla'}
+                        styles={styles}></Birthday>
                 </div>
-                <Birthday
-                    state={description}
-                    savable={savable}
-                    setSavable={setSavable}
-                    styles={styles}></Birthday>
             </div>
-        </div>
+            <button
+                onClick={() => {
+                    putUser(putUserUrl, accessToken, dispatch, {
+                        text: profile.text,
+                    })
+                }}
+                className={`${styles.btnClass} ${
+                    true ? 'bg-teal-dark' : 'bg-teal-bright cursor-not-allowed'
+                }`}>
+                Save
+            </button>
+        </section>
     )
 }
