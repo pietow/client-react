fd86015f9c99df5756ae36e132251603ac460f09
Sonny Stark <sonny.stark@posteo.net>
2022-04-25 16:45:32 +0200

    feat: language-list and hide data if not filled out

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,28 +1,69 @@
 /** @format */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function ProfileCard({ userData }) {
+    const [visibleData, setVisibleData] = useState(null)
+
+    useEffect(() => {
+        setVisibleData(true)
+    }, [])
+
+    const languages = userData.language
+
+    const displayLanguage = languages.map((language, i) => {
+        return (
+            <li key={i}>
+                <span> - </span>
+                {language}
+            </li>
+        )
+    })
+
     console.log(userData)
     return (
         <card className="w-1/5 h-full border-2 border-gray-dark p-4 rounded bg-best-white">
             <div className="">
-                {/* <figure>Profile image goes here</figure> */}
-                <p className="text-black text-xs border-b-2 p-2">
-                    Username: {userData.username}
-                </p>
-                <p className="text-black text-xs border-b-2 p-2">
-                    Name: {userData.fname + ' ' + userData.lname}
-                </p>
-                <p className="text-black text-xs border-b-2 p-2">
-                    Gender: {userData.gender}
-                </p>
-                <p className="text-black border-b-2 p-2 text-xs">
-                    Age: {userData.birthday}
-                </p>
-                <p className="text-black border-b-2 p-2 text-xs">
-                    Lives in {userData.city + ', ' + userData.country}
-                </p>
+                <img
+                    className="pb-10 m-auto justify-center"
+                    src={'https://picsum.photos/200/200.jpg'}
+                    alt="lorem"
+                />
+                {visibleData && (
+                    <p className="text-black text-xs border-b-2 p-2">
+                        @{userData.username}
+                    </p>
+                )}
+                {visibleData && (
+                    <p className="text-black text-xs border-b-2 p-2">
+                        Member since {userData.createdAt}
+                    </p>
+                )}
+                {visibleData && (
+                    <p className="text-black text-xs border-b-2 p-2">
+                        Name: {userData.fname + ' ' + userData.lname}
+                    </p>
+                )}
+                {visibleData && (
+                    <p className="text-black text-xs border-b-2 p-2">
+                        Gender: {userData.gender}
+                    </p>
+                )}
+                {visibleData && (
+                    <p className="text-black border-b-2 p-2 text-xs">
+                        Age: {userData.birthday}
+                    </p>
+                )}
+                {visibleData && (
+                    <p className="text-black border-b-2 p-2 text-xs">
+                        Lives in {userData.city + ', ' + userData.country}
+                    </p>
+                )}
+                {visibleData && (
+                    <div className="text-black border-b-2 p-2 text-xs">
+                        Languages: {displayLanguage}
+                    </div>
+                )}
             </div>
         </card>
     )
