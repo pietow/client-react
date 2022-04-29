77f8c31dd9604348b8b47c439717e94c2698b8af
gino <gino.massalski@gmail.com>
2022-04-29 10:04:55 +0200

    bugFix: fix state after login

--- src/util/fetchUser.js
+++ src/util/fetchUser.js
@@ -23,20 +23,10 @@
             const result = await response.json()
             const profile = result.profile
             const accommodation = result.accommodation
-            delete result.profile
-            delete result.accommodation
             if (!result.error && verb !== 'POST') {
                 dispatch({
                     type: 'update_user',
                     payload: result,
-                })
-                dispatch({
-                    type: 'update_profile',
-                    payload: profile,
-                })
-                dispatch({
-                    type: 'update_accommodation',
-                    payload: accommodation,
                 })
             }
             return new Promise((resolve, reject) => {
